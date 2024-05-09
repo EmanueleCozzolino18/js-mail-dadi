2f3824114019df533cc183ebfb31a9193eac321b
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-09 14:25:26 +0200

    [UPD] Random dice generation for computer and user

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,3 +2,14 @@
 "use strict";
 
 console.log("Script caricato correttamente!");
+
+
+//ANDIAMO A GENERARE I NUMERI DA UNO A SEI PER UTENTE E PER COMPUTER
+const dadoUtente = Math.floor((Math.random() * 6) + 1);
+
+console.log('User Roll: ', dadoUtente);
+
+const dadoComputer = Math.floor((Math.random() * 6) + 1);
+
+console.log('Computer Roll: ', dadoComputer);
+
