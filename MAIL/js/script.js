1bbd02436bb86d224ea6c952ef8371a2127c9a3d
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-09 14:45:35 +0200

    [UPD] Enter the conditions to validate access

--- MAIL/js/script.js
+++ MAIL/js/script.js
@@ -10,3 +10,19 @@ const emailCheck = ['emanuele.cozzolino18@libero.it', 'paolo.dunzioni@libero.it'
 
 console.log(email)
 
+//andiamo a creare questa variabile booleana che andremo a manipolare successivamente
+let mailValidate = false;
+
+//Conforinatiamo la mail inserita dal utente se combacia con una presente nel nostro array la nostra variabile booleana diventerà vera
+for (let i = 0; i < emailCheck.length; i++) {
+
+    if (email === emailCheck[i]) {
+        mailValidate = true;
+        break;
+    }
+}
+
+if (mailValidate === true) console.log("Mail valida accesso in corso"); //Se la nostra varabile sarà diventata vera la mail è uguale e quindi possiamo entrare
+else console.log("Mail non valida!"); //Altrimenti nop
+
+
