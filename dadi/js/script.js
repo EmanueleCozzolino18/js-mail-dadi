e0da2ac5f4a0fcff95bf7812c33468045e01f5ce
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-09 14:31:29 +0200

    [UPD] Created the condition to test the number greater than or equal

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -13,3 +13,7 @@ const dadoComputer = Math.floor((Math.random() * 6) + 1);
 
 console.log('Computer Roll: ', dadoComputer);
 
+//ANDIAMO A CREARE LA CONDIZIONE PER VERIFICARE IL NUMERO VINCENTE
+if (dadoUtente > dadoComputer) console.log(`L'utente ha vinto ${dadoUtente} > ${dadoComputer}`) //IN CASO IL NUMERO UTENTE E' MAGGIORE DEL COMPUTER L'UTENTE VINCE
+else if (dadoUtente < dadoComputer) console.log(`Il computer ha vinto ${dadoComputer} > ${dadoUtente}`) //IN CASO IL NUMERO DEL COMPUTER E' MAGGIORE IL COMPUTER VINCE 
+else console.log(`Entrambi i numeri sono uguali ${dadoUtente} = ${dadoComputer}`) //ALTRIMENTI SE SONO PARI NESSUNO VINCE
