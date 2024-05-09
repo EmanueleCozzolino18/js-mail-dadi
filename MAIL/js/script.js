d9d4ea95ea164a43e13b8766e3596ddcbc1d3c54
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-09 14:39:57 +0200

    [UPD] We record user input

--- MAIL/js/script.js
+++ MAIL/js/script.js
@@ -2,3 +2,11 @@
 "use strict";
 
 console.log("Script caricato correttamente!");
+
+//Andiamo a leggere la mail inserita dal utente e l assegnamo alla variabile email
+let email = prompt("Inserisci la tua mail: ");
+//Creiamo un array con le mail che il nostro programma potrà accettare
+const emailCheck = ['emanuele.cozzolino18@libero.it', 'paolo.dunzioni@libero.it', 'cristian.cosenza@gmail.com', 'gaetano.frascolla@gmail.com'];
+
+console.log(email)
+
